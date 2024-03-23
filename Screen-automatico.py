081d2f4f6e7f22303f5c3132168f2d349059d578
kevinalexander <kevinalexander.ccm@gmail.comgit config --global user.email kevinalexander.ccm@gmail.comgit config --global user.email kevinalexander.ccm@gmail.com>
2024-03-23 12:16:47 -0300

    Captura de pantalla

---
+++ Screen-automatico.py
@@ -0,0 +1,67 @@
+"""
+Screen shoot 
+
+"""
+
+import time
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from urllib.parse import urlparse
+from urllib3.exceptions import LocationParseError
+
+def check_websites(urls):
+    pages_loaded = 0
+    pages_failed = 0
+    
+    for url in urls:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                print(f"La página {url} se cargó correctamente.")
+                take_screenshot(url)
+                pages_loaded += 1
+            else:
+                print(f"La página {url} no se pudo cargar (código de estado: {response.status_code}).")
+                pages_failed += 1
+        except requests.exceptions.RequestException as e:
+            print(f"Error al acceder a la página {url}: {str(e)}")
+            pages_failed += 1
+        except LocationParseError:
+            print(f"Error de URL inválida para la página {url}.")
+            pages_failed += 1
+
+    print(f"\nCantidad de páginas cargadas correctamente: {pages_loaded}")
+    print(f"Cantidad de páginas que no se pudieron cargar: {pages_failed}")
+
+def take_screenshot(url):
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
+    
+    try:
+        driver.get(url)
+        time.sleep(2)  # Esperar 2 segundos para que la página se cargue completamente
+        
+        parsed_url = urlparse(url)
+        filename = parsed_url.netloc + parsed_url.path
+        filename = filename.replace("/", "_")  # Reemplazar "/" con "_" en la ruta para evitar problemas con el nombre del archivo
+        driver.save_screenshot(f"screenshot_{filename}.png")
+        driver.quit()
+        
+        print(f"Se tomó el screenshot de {url} correctamente.")
+    except Exception as e:
+        print(f"Error al tomar el screenshot de {url}: {str(e)}")
+        driver.quit()
+
+
+# Lista de URLs de las páginas que deseas comprobar
+urls = [
+    
+]
+check_websites(urls)
+
+# Imprimir los resultados finales y agregar una pausa para mantener la consola abierta
+print("\nProceso completado.")
+input("Presiona Enter para salir...")
+
